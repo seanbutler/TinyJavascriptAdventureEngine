effff843f9c5f1ba40b620ed40ecf5bd524fa893
Sean Butler <butlersean@gmail.com>
2019-06-02 19:27:04 +0100

    simple game

--- SimpleAdventureGame/Fortnite/gamedata.js
+++ SimpleAdventureGame/Fortnite/gamedata.js
@@ -19,89 +19,25 @@ var gamedata = {
   "states":[
       {
           // 0
-          "description":"You are standing in the hallway, trying to go to the allotment.",
+          "description":"All the kids at school are telling you they spent all weekend playing Fortnite even though they are only 8.",
           "events":[
                 {"title":"Wait","response":1},
-                {"title":"Go Through the Door","response":2}
+                {"title":"Ask Parents","response":2}
               ]
       },
       {
           // 1
-          "description":"There are some noises from elsewhere in the house, not sure what is happening.",
+          "description":"At this rate it will take years (4 literally) before you can play Fortnite.",
           "events":[
-                {"title":"Wait","response":0},
-                {"title":"Call Out","response":3},
-              ]
+              {"title":"OK","response":0}
+          ]
       },
       {
           // 2
-          "description":"Dude, you cant leave a little one alone in the house. I'm calling social services on you.",
+          "description":"you wait till dad gets home, and ask him, but dad works in the games industry and knows how compelling these monetised games can be, also he points out you are too young and the classification is for 12 year olds and up. So no dice kiddo.",
           "events":[
-              {"title":"Start Over","response":0}
-          ]
-      },
-      {
-          // 3
-          "description":"You and the littlest one are standing in the hallway, trying to go to the allotment.",
-          "events":[
-                {"title":"Wait","response":4},
-                {"title":"Go Through the Door","response":6},
+                {"title":"Dang","response":0},
               ]
-      },
-      {
-          // 4
-          "description":"You hear a whining noise and look around, its coming from the small person.",
-          "events":[
-                {"title":"Whats up little one?","response":5},
-                {"title":"Go Through the Door","response":7},
-              ]
-      },
-      {
-          // 5
-          "description":"She is dancing on the spot and looking around",
-          "events":[
-              {"title":"Food?","response":4},
-              {"title":"Water?","response":4},
-              {"title":"Toilet?","response":6},
-              ]
-      },
-      {
-          // 6
-          "description":"You visit the bathroom. She uses the facilities, you help wash hands.",
-          "events":[
-              {"title":"Back to the hallway","response":10}
-              ]
-      },
-      {
-          // 7
-          "description":"The noise rises in pitch and intensity",
-          "events":[
-              {"title":"Err","response":4}
-              ]
-      },
-      {
-          // 8
-          "description":"You are both outside the house, the door is closed, the driveway has weeds growning on it. There is a street to the south.",
-          "events":[
-                { "title":"Go In","response":0},
-                { "title":"Go South","response":9},
-              ]
-      },
-      {
-          // 9
-          "description":"Finally You are at the allotment! Now pick some vegetables.",
-          "events":[
-              {"title":"Start Over","response":0}
-          ]
-      },
-      {
-          // 10
-          "description":"You and the littlest one are standing in the hallway. Everything seems normal.",
-          "events":[
-                {"title":"Wait","response":4},
-                {"title":"Go Through the Door","response":8},
-              ]
-      },
-    ]
+      }    ]
 
   };
